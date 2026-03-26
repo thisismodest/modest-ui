52bdeada649a95e6b21d1d32a05a297fca73c2b4
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 23:54:40 +0000

    Add skeletons

---
+++ components/skeleton/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Skeleton",
+  className: ".mdst-skeleton",
+  description:
+    "Placeholder loading shapes with an animated shimmer effect. Supports text, heading, and circle variants for building loading states.",
+};
