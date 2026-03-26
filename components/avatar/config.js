49bc90197f92477fd0947f8eaa867891fc9a0cb8
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 22:35:12 +0000

    Add Avatar component

---
+++ components/avatar/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Avatar",
+  className: ".mdst-avatar",
+  description:
+    "Circular or square avatar containers for user images and initials. Supports multiple sizes and grouping with overlap.",
+};
