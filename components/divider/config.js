ffdd12181f71b72aa8ca6db44bfec21fd0dcae50
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 23:28:46 +0000

    Remove divider as it was conflicting with the core philosophy

--- components/divider/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  title: "Divider",
-  className: ".mdst-divider",
-  description:
-    "Horizontal and vertical separators for content. Supports subtle, thick, vertical, and text divider variants.",
-};
