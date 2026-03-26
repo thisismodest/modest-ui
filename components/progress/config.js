ea2a2324a9f90e35f8bb8486907b335688d38b8a
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 23:50:52 +0000

    Add progress component

---
+++ components/progress/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Progress",
+  className: ".mdst-progress",
+  description:
+    "Progress bar component styling the native <progress> element with size and status variants.",
+};
