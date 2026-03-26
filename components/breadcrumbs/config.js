50ed2cad2d31e69dac00eb850ed84266cb731e49
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 22:56:26 +0000

    Add breadcrumbs

---
+++ components/breadcrumbs/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Breadcrumbs",
+  className: ".mdst-breadcrumbs",
+  description:
+    "Navigation breadcrumb trail with separator support. Works classless via aria-label or with explicit class.",
+};
