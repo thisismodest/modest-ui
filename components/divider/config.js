1bea43fcb6055fbfcf32173f12ddc5237723f2a4
Marcus Michaels <marcus@modestindustries.co>
2026-03-26 23:03:24 +0000

    Add divider component

---
+++ components/divider/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Divider",
+  className: ".mdst-divider",
+  description:
+    "Horizontal and vertical separators for content. Supports subtle, thick, vertical, and text divider variants.",
+};
