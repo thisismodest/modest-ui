3580cb2d8dc3af2c27021bb97a9e0de052ea26e2
Marcus Michaels <marcus@modestindustries.co>
2026-03-27 00:54:32 +0000

    Add toast component

---
+++ components/toast/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Toast",
+  className: ".mdst-toast",
+  description:
+    "Notification toast messages with semantic variants, driven by data attributes for visibility and variant state.",
+};
