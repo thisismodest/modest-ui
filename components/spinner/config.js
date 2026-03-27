70d8c144627bbcaf42e039a4f46d420ae6625737
Marcus Michaels <marcus@modestindustries.co>
2026-03-27 00:03:27 +0000

    Add loading spinner

---
+++ components/spinner/config.js
@@ -0,0 +1,6 @@
+export default {
+  title: "Spinner",
+  className: ".mdst-spinner",
+  description:
+    "Animated loading spinner using CSS borders and rotation. Inherits color from context with size variants.",
+};
